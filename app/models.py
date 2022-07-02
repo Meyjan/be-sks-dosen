4fb941f04b5b948fcd7830a6cc23a1a155a3e698
Jan Meyer Saragih <saragih.meyer0821@gmail.com>
2022-07-02 20:58:49 +0700

    Add table user

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
+from operator import length_hint
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), index=True, unique=True)
-    email = db.Column(db.String(100), index=True, unique=True)
-    password = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    username = db.Column(db.String(length=100), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(length=100), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(length=100), nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABSE_URI = os.environ.get('DATABASE_URL') or 'mysql://root@localhost:3306/db-sks-dosen'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root@localhost:3306/db-sks-dosen'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
